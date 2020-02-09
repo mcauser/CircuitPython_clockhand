c4c7664f3251d360688cf85ada9ed20266519ebb
Lee Nelson <lnelson@nelnet.org>
2020-02-08 22:50:39 -0800

    Comment out doc build from workflow

--- circuitpython_clockhand.py
+++ circuitpython_clockhand.py
@@ -34,7 +34,7 @@ Implementation Notes
 **Hardware:**
 
 .. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
-   inline format: "* `Link Text <url>`_"
+#   inline format: "* `Link Text <url>`_"
 
 **Software and Dependencies:**
 
